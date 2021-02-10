f1efe7ce1ed2a2b453b85d3b0c18f3d8bee00b5f
Weng Fei Fung <weffung@ucdavis.edu>
2021-02-10 04:51:02 -0800

    Toggle descriptions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
 console.info("%cApp init: %cPassed", "font-weight:bold", "font-style:italic; color:green;");
+
+/**
+ * Eye icon that toggles display of description
+ */
+function toggleDescription(event, here) {
+    var context = here.closest("figure");
+    var descs = context.querySelector("figcaption");
+    descs.classList.toggle("display-none");
+    event.preventDefault();
+    event.stopPropagation();
+}
